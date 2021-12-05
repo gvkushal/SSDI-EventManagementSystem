a9ac38793a5b5364a1518816c9c31062a0a6312a
unknown <kushalcool70@gmail.com>
2021-12-05 13:12:11 -0500

    refactored delete api

--- Backend/src/main/java/com/event/management/controller/EventController.java
+++ Backend/src/main/java/com/event/management/controller/EventController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.event.management.model.Event;
@@ -57,7 +58,7 @@ public class EventController {
 
 	@DeleteMapping("/delete/{eventId}")
 	@ApiOperation(value = "Delete an existing event")
-	public void deleteEventById(@PathParam("eventId") int eventId) {
+	public void deleteEventById(@RequestParam int eventId) {
 		eventService.deleteEventById(eventId);
 	}
 
